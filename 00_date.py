da4e2ebf3961d4933ce03b45f46efed23275132c
skylit <cesaralonso227@gmail.com>
2022-12-09 17:01:38 -0500

    dates

---
+++ 00_date.py
@@ -0,0 +1,17 @@
+### Dates ###
+
+from datetime import datetime
+
+now = datetime.now()
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now)
+
+
+timestamp = now.timestamp()
+
+print(timestamp)
